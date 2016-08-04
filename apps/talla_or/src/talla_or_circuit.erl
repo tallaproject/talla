a652d79176c4296b78b975f0ac4f13552fdce0b5
Alexander Færøy <ahf@0x90.dk>
2016-08-04 02:33:19 +0200

    Decode encrypted cell's upon arrival at the circuit

--- apps/talla_or/src/talla_or_circuit.erl
+++ apps/talla_or/src/talla_or_circuit.erl
@@ -12,9 +12,8 @@
 -behaviour(gen_statem).
 
 %% API.
--export([start_link/0,
+-export([start_link/1,
          stop/1,
-         controlling_process/2,
          dispatch/2
         ]).
 
@@ -35,9 +34,6 @@
 -type t() :: pid().
 
 -record(state, {
-        %% Control Process.
-        control_process :: pid(),
-
         %% The ID of our circuit.
         %% FIXME(ahf): Should be something like onion_circuit:id().
         circuit :: non_neg_integer(),
@@ -60,26 +56,19 @@
 %% API.
 %% ----------------------------------------------------------------------------
 
--spec start_link() -> {ok, Circuit} | {error, Reason}
+-spec start_link(CircuitID) -> {ok, Circuit} | {error, Reason}
     when
-        Circuit :: t(),
-        Reason  :: term().
-start_link() ->
-    gen_statem:start_link(?MODULE, [], []).
+        CircuitID :: non_neg_integer(),
+        Circuit   :: t(),
+        Reason    :: term().
+start_link(CircuitID) ->
+    gen_statem:start_link(?MODULE, [CircuitID], []).
 
 -spec stop(Circuit) -> ok
     when
         Circuit :: t().
 stop(Circuit) ->
     gen_statem:stop(Circuit).
-
-%% FIXME(ahf): controlling_peer?
--spec controlling_process(Circuit, Peer) -> ok
-    when
-        Circuit :: t(),
-        Peer    :: talla_or_peer:t().
-controlling_process(Circuit, Pid) ->
-    gen_statem:cast(Circuit, {controlling_process, Pid}).
 
 -spec dispatch(Circuit, Cell) -> ok
     when
@@ -103,7 +92,7 @@ create(internal, {cell, #{ command := create2,
                            payload := #{ data := <<Fingerprint:20/binary,
                                                    NTorPublicKey:32/binary,
                                                    PublicKey:32/binary>>
-                                        }}}, #state { circuit = undefined } = StateData) ->
+                                        }}}, StateData) ->
     %% We received a create2 cell and haven't already gotten a Circuit ID.
     %% Update our state and reply to the create request.
     lager:notice("Creating new relay: ~b", [Circuit]),
@@ -116,7 +105,11 @@ create(internal, {cell, #{ command := create2,
 
             %% Update our state.
             NewStateData = StateData#state {
-                circuit = Circuit
+                circuit          = Circuit,
+                forward_aes_key  = maps:get(forward_aes_key, HandshakeState),
+                backward_aes_key = maps:get(backward_aes_key, HandshakeState),
+                forward_hash     = maps:get(forward_hash, HandshakeState),
+                backward_hash    = maps:get(backward_hash, HandshakeState)
             },
 
             %% Continue to the normal loop.
@@ -134,8 +127,41 @@ create(EventType, EventContent, StateData) ->
         EventType    :: gen_statem:event_type(),
         EventContent :: term(),
         StateData    :: state().
+relay(internal, {cell, #{ command := relay_early,
+                          circuit := Circuit,
+                          payload := #{ data := Payload } }}, #state { circuit           = Circuit,
+                                                                       forward_aes_key   = FAESKey,
+                                                                       forward_hash      = FHash,
+                                                                       relay_early_count = RelayEarlyCount } = StateData) ->
+    case decrypt(FAESKey, FHash, Payload) of
+        {recognized, NewAESKey, #{ command := 14,
+                                   stream  := 0,
+                                   payload := #{ data  := Data,
+                                                 type  := ntor,
+                                                 links := [#{ type    := tls_over_ipv4,
+                                                              payload := {Address, Port} }, _] }}} ->
+            lager:warning("Bueno"),
+            talla_or_peer_manager:connect(Address, Port);
+
+        relay ->
+            lager:warning("Relay");
+
+        NoMatch ->
+            lager:warning("No match: ~p", [NoMatch])
+    end,
+
+    %% Update our state.
+    NewStateData = StateData#state {
+        relay_early_count = RelayEarlyCount + 1
+    },
+
+    {keep_state, NewStateData};
+
 relay(EventType, EventContent, StateData) ->
     handle_event(EventType, EventContent, StateData).
+
+await_peer_connect(EventType, EventContent, StateData) ->
+    {keep_state, StateData, [postpone]}.
 
 %% ----------------------------------------------------------------------------
 %% Generic State Machine Callbacks.
@@ -143,14 +169,13 @@ relay(EventType, EventContent, StateData) ->
 
 %% @private
 %% This function is used to initialize our state machine.
-init([]) ->
+init([CircuitID]) ->
     %% We want to trap exit signals.
     process_flag(trap_exit, true),
 
     %% Our initial state.
     StateData = #state {
-        forward_hash  = crypto:hash_init(sha),
-        backward_hash = crypto:hash_init(sha)
+        circuit = CircuitID
     },
 
     %% We start in the create state.
@@ -200,24 +225,15 @@ handle_event(internal, {cell_sent, _Cell}, StateData) ->
     {keep_state, StateData};
 
 handle_event(cast, {dispatch, #{ command := Command,
-                                 circuit := Circuit } = Cell}, StateData) ->
+                                 circuit := Circuit } = Cell}, #state { circuit = Circuit } = StateData) ->
     lager:notice("Relay: ~p (Circuit: ~b)", [Command, Circuit]),
     {keep_state, StateData, [{next_event, internal, {cell, Cell}}]};
 
-handle_event(cast, {send, #{ command := Command,
-                             circuit := Circuit } = Cell}, #state { control_process = ControlProcess } = StateData) ->
-    lager:notice("Relay Response: ~p (Circuit: ~b)", [Command, Circuit]),
-    talla_or_peer:send(ControlProcess, Cell),
-    {keep_state, StateData, [{next_event, internal, {cell_sent, Cell}}]};
-
-handle_event(cast, {controlling_process, Pid}, StateData) ->
-    %% Set our control process.
-
-    NewStateData = StateData#state {
-        control_process = Pid
-    },
-
-    {keep_state, NewStateData};
+%handle_event(cast, {send, #{ command := Command,
+%                             circuit := Circuit } = Cell}, #state { control_process = ControlProcess } = StateData) ->
+%    lager:notice("Relay Response: ~p (Circuit: ~b)", [Command, Circuit]),
+%    talla_or_peer:send(ControlProcess, Cell),
+%    {keep_state, StateData, [{next_event, internal, {cell_sent, Cell}}]};
 
 handle_event(EventType, EventContent, StateData) ->
     %% Looks like none of the above handlers was able to handle this message.
@@ -264,8 +280,8 @@ ntor_handshake(Fingerprint, PublicKey, ClientPublicKey) ->
 
             {ok, #{ response => Response,
 
-                    forward_hash  => FHash,
-                    backward_hash => BHash,
+                    forward_hash  => crypto:hash_update(crypto:hash_init(sha), FHash),
+                    backward_hash => crypto:hash_update(crypto:hash_init(sha), BHash),
 
                     forward_aes_key  => onion_aes:init(FKey),
                     backward_aes_key => onion_aes:init(BKey) }};
@@ -273,3 +289,66 @@ ntor_handshake(Fingerprint, PublicKey, ClientPublicKey) ->
         false ->
             {error, key_mismatch}
     end.
+
+%% @private
+digest(Context, Data) ->
+    <<Start:5/binary, _:32/integer, Rest/binary>> = Data,
+    <<Result:4/binary, _/binary>> = crypto:hash_final(crypto:hash_update(Context, <<Start/binary, 0:32/integer, Rest/binary>>)),
+    Result.
+
+%% @private
+decrypt(AESKey, Context, Payload) ->
+    {NewAESKey, Message} = onion_aes:decrypt(AESKey, Payload),
+    MessageDigest = digest(Context, Message),
+    case Message of
+        <<Command:8/integer, 0:16/integer, StreamID:16/integer, MessageDigest:4/binary, Length:16/integer, Packet:Length/binary, _/binary>> ->
+            {recognized, NewAESKey, #{ command => Command,
+                                       stream  => StreamID,
+                                       payload => decode_relay_packet(Command, Packet) } };
+
+        _ ->
+            relay
+    end.
+
+%% @private
+decode_relay_packet(14, Payload) ->
+    %% RELAY_EXTEND2
+    decode_extend2_cell(Payload);
+
+decode_relay_packet(_, _) ->
+    unknown.
+
+decode_htype(0) ->
+    tap;
+decode_htype(2) ->
+    ntor;
+decode_htype(_) ->
+    unknown.
+
+decode_link_type(0) ->
+    tls_over_ipv4;
+decode_link_type(1) ->
+    tls_over_ipv6;
+decode_link_type(2) ->
+    legacy_identity;
+decode_link_type(_) ->
+    unknown.
+
+decode_extend2_cell(<<NSpec:8/integer, Rest/binary>>) ->
+    {Links, <<HType:16/integer, HLen:16/integer, HData:HLen/bytes, _/binary>>} = decode_extend2_link_specifiers(Rest, [], NSpec),
+    #{ type => decode_htype(HType), data => HData, links => Links }.
+
+decode_extend2_link_specifiers(Rest, Result, 0) ->
+    {lists:reverse(Result), Rest};
+
+decode_extend2_link_specifiers(<<LSType:8/integer, LSLen:8/integer, LSpec:LSLen/binary, Rest/binary>>, Result, NSpec) ->
+    decode_extend2_link_specifiers(Rest, [#{ type => decode_link_type(LSType), payload => decode_extend2_link_specifier(LSType, LSpec) } | Result], NSpec - 1).
+
+decode_extend2_link_specifier(0, <<A:8/integer, B:8/integer, C:8/integer, D:8/integer, Port:16/integer>>) ->
+    {{A, B, C, D}, Port};
+
+decode_extend2_link_specifier(1, <<A:16/integer, B:16/integer, C:16/integer, D:16/integer, E:16/integer, F:16/integer, G:16/integer, H:16/integer, Port:16/integer>>) ->
+    {{A, B, C, D, E, F, G, H}, Port};
+
+decode_extend2_link_specifier(2, <<Fingerprint:20/binary, _/binary>>) ->
+    Fingerprint.
